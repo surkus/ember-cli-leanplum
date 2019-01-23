38cef9e2d8817ed6d9cb2508e7f0ed38071b4f18
Larry Sprock <larry@lucidbleu.com>
2019-01-23 12:21:23 -0800

    Update to use ember fastboot in different manor

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
 'use strict';
 
-var path = require('path');
-var fastbootTransform = require('fastboot-transform');
+const path = require('path');
+const fastbootTransform = require('fastboot-transform');
+const Funnel = require('broccoli-funnel');
+const mergeTrees = require('broccoli-merge-trees');
+const resolve = require('resolve');
 
 module.exports = {
-  name: 'ember-cli-leanplum',
+  name: require('./package').name,
 
-  included: function (app) {
+  included: function () {
     this._super.included.apply(this, arguments);
-    var libPath = path.join('node_modules', 'leanplum-sdk', 'dist', 'leanplum.js');
-    var importOptions = {
-      using: [{
-        transformation: 'fastbootTransform'
-      }]
-    };
+    this._ensureFindHost();
 
-    app.import(libPath, importOptions);
+    let vendorPath = `vendor/${this.name}`;
+    let host = this._findHost();
+
+    host.import(path.join(vendorPath, 'leanplum.js'));
   },
 
-  importTransforms: function () {
-    return {
-      fastbootTransform: fastbootTransform
+  treeForVendor() {
+    let leanplumPath = path.join(this.resolvePackagePath('leanplum-sdk'), 'dist');
+
+    let leanplumJs = fastbootTransform(new Funnel(leanplumPath, {
+      files: ['leanplum.js'],
+      destDir: this.name
+    }));
+
+    return mergeTrees([leanplumJs]);
+  },
+
+  resolvePackagePath(packageName) {
+    let host = this._findHost();
+    return path.dirname(resolve.sync(`${packageName}/package.json`, { basedir: host.project.root }));
+  },
+
+  _ensureFindHost() {
+    if (!this._findHost) {
+      this._findHost = function findHostShim() {
+        let current = this;
+        let app;
+
+        do {
+          app = current.app || app;
+        } while (current.parent.parent && (current = current.parent));
+
+        return app;
+      };
     }
   }
 };
